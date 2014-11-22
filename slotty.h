813838c9f04a2a27a1f6e29b593c07a79ba1ca59
Pranjal Raihan <prshreshtha@yahoo.com>
2014-11-21 21:52:48 -0500

    Converted tabs to spaces

--- slotty.h
+++ slotty.h
@@ -31,76 +31,76 @@
 
 namespace slotty
 {
-	template <typename... contexts>
-	class event;
+    template <typename... contexts>
+    class event;
 
-	template <typename... contexts>
-	class slot final
-	{
-	public:
+    template <typename... contexts>
+    class slot final
+    {
+    public:
         /**
          * \typedef the callback function type
          */
-		typedef std::function<void(contexts...)> callback_type;
+        typedef std::function<void(contexts...)> callback_type;
 
-	private:
+    private:
         /**
          * \brief the default constructor
          */
-		explicit slot() = default;
+        explicit slot() = default;
 
         /**
          * \brief no copy
          */
-		explicit slot(const slot<contexts...>&) = delete;
+        explicit slot(const slot<contexts...>&) = delete;
 
         /**
          * \brief no assign
          */
-		slot<contexts...>& operator=(const slot<contexts...>&) = delete;
+        slot<contexts...>& operator=(const slot<contexts...>&) = delete;
 
-	public:
+    public:
         /**
          * \brief the non-virtual destructor
          */
-		~slot();
+        ~slot();
 
-	private:
+    private:
         /**
          * \brief pointer to associated event
          */
-		const event<contexts...>* event_;
+        const event<contexts...>* event_;
 
         /**
          * \brief the callback function
          */
-		callback_type callback_;
+        callback_type callback_;
 
-		friend class event<contexts...>;
-	};
+        friend class event<contexts...>;
+    };
 
-	template <typename... contexts>
-	class event final
-	{
-	public:
+    template <typename... contexts>
+    class event final
+    {
+    public:
         /**
          * \typedef the associated slot type
          */
-		typedef typename slot<contexts...> slot_type;
+        typedef typename slot<contexts...> slot_type;
         /**
          * \typedef the callback function type
          */
-		typedef typename slot_type::callback_type callback_type;
+        typedef typename slot_type::callback_type callback_type;
 
         /**
          * \brief the default constructor
          */
-		explicit event() = default;
+        explicit event() = default;
 
         /**
         * \brief the non-virtual destructor
         */
-		~event() = default;
+        ~event() = default;
 
         /**
          * \brief add a listener of to the list of listeners
@@ -108,76 +108,76 @@ namespace slotty
          * The lifetime of the listener is tied to the slot returned. 
          * When the slot is deleted (destructor is called), the 
          * listener is also invalidated. The slot returned is completely 
-		 * owned by the caller and must be delete'd by the user.
+         * owned by the caller and must be delete'd by the user.
          *
          * \return the slot object for listener lifetime control
          */
-		slot_type* connect(
-			callback_type callback //!< [in] the callback function
-		) const;
+        slot_type* connect(
+            callback_type callback //!< [in] the callback function
+        ) const;
 
         /**
          * \brief raise the event
          *
          * This method dispatches a message to all listeners.
          */
-		void raise(
-			contexts... args //!< [in] the event arguments
-		);
+        void raise(
+            contexts... args //!< [in] the event arguments
+        );
 
-	private:
+    private:
         /**
          * \brief no copy
          */
-		explicit event(const event<contexts...>&) = delete;
+        explicit event(const event<contexts...>&) = delete;
 
         /**
          * \brief no assign
          */
-		event<contexts...>& operator=(const event<contexts...>&) = delete;
+        event<contexts...>& operator=(const event<contexts...>&) = delete;
 
-	private:
+    private:
         /**
          * \brief list of listeners
          */
-		mutable std::vector<slot_type*> listeners_;
+        mutable std::vector<slot_type*> listeners_;
 
-		friend class slot<contexts...>;
-	};
+        friend class slot<contexts...>;
+    };
 
 
 
 
     // template implementation
 
-	template <typename... contexts>
-	typename event<contexts...>::slot_type* event<contexts...>::connect(
-		typename event<contexts...>::callback_type callback
-	) const
-	{
-		auto s = new typename event<contexts...>::slot_type();
-		s->event_ = this;
-		s->callback_ = callback;
-		this->listeners_.push_back(s);
-		return s;
-	}
+    template <typename... contexts>
+    typename event<contexts...>::slot_type* event<contexts...>::connect(
+        typename event<contexts...>::callback_type callback
+    ) const
+    {
+        auto s = new typename event<contexts...>::slot_type();
+        s->event_ = this;
+        s->callback_ = callback;
+        this->listeners_.push_back(s);
+        return s;
+    }
 
-	template <typename... contexts>
-	void event<contexts...>::raise(contexts... args)
-	{
-		for (auto i = this->listeners_.begin(); i != this->listeners_.end(); ++i)
-		{
-			auto& f = *(*i);
-			f.callback_(args...);
-		}
-	}
+    template <typename... contexts>
+    void event<contexts...>::raise(contexts... args)
+    {
+        for (auto i = this->listeners_.begin(); i != this->listeners_.end(); ++i)
+        {
+            auto& f = *(*i);
+            f.callback_(args...);
+        }
+    }
 
-	template <typename... contexts>
-	slot<contexts...>::~slot()
-	{
-		auto& listeners = this->event_->listeners_;
-		listeners.erase(std::remove(listeners.begin(), listeners.end(), this), listeners.end());
-	}
+    template <typename... contexts>
+    slot<contexts...>::~slot()
+    {
+        auto& listeners = this->event_->listeners_;
+        listeners.erase(std::remove(listeners.begin(), listeners.end(), this), listeners.end());
+    }
 
 }
 
